bd61460616e7863f4534fbe5bfdbccf635cee28b
kulvanth-dev <kulvanthsaimaddala@gmail.com>
2025-02-19 09:30:59 +0530

    Update settings.py

--- weatherproject/settings.py
+++ weatherproject/settings.py
@@ -19,13 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-import environ
-
-#Initialise environment variables
-
-env = environ.Env()
-
-environ.Env.read_env()
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
